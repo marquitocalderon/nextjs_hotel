901f36718d82292e9ed368a7763ffe29175eefe7
Marco Calderon <marco.calderon1701@hotmail.com>
2023-12-09 17:41:52 -0500

    con la api en registro esta

--- src/app/registrarse/page.jsx
+++ src/app/registrarse/page.jsx
@@ -109,7 +109,7 @@ export default function Registrarse() {
 
       try {
         const response = await axios.post(
-          "http://localhost:4000/registro-cliente",
+          "https://backendprueba-9rnj.onrender.com/registro-cliente",
           enviarDatos
         );
 
