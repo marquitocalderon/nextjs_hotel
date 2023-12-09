09a183339f8dcb3c433052c3e5ccde9e8e7147af
Marco Calderon <marco.calderon1701@hotmail.com>
2023-12-09 14:45:49 -0500

    prueba rutas

---
+++ src/app/page.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>
+      <a href="/login">LOGIN</a>
+      <a href="/registrarse">Registro</a>
+    </div>
+  )
+}
