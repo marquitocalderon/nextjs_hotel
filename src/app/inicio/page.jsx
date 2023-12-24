b4ed0834264dbffa597bab947e037d654e26bfba
Marco Calderon <marco.calderon1701@hotmail.com>
2023-12-24 18:39:48 -0500

    laa fe actualizado

--- src/app/inicio/page.jsx
+++ src/app/inicio/page.jsx
@@ -1,15 +1,22 @@
 "use client"
-import React from 'react'
-import {  useSession, signOut } from 'next-auth/react'
-export default function page() {
+import React from 'react';
+import { useSession, signOut } from 'next-auth/react';
 
-  const { data: session } = useSession()
+const Page = () => {
+  const { data: session } = useSession();
+
+  const handleSignOut = () => {
+    signOut({ callbackUrl: '/' });
+  };
+
   return (
     <div>
-            Hola {session?.user?.name} {session?.user?.email}
-            <button className='px-4 py-2 bg-blue-500 text-center' onClick={ async ()=>  await signOut({callbackUrl: "/"})}>
-          CERRAR GOOGLE
-        </button>
+      Hola {session?.user?.name} {session?.user?.email}
+      <button className='px-4 py-2 bg-blue-500 text-center' onClick={handleSignOut}>
+        CERRAR GOOGLE
+      </button>
     </div>
-  )
-}
+  );
+};
+
+export default Page;
